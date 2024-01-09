a15ac685a22b92c3d4d733fed894c5aa6cba11e8
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-01-09 16:25:36 +0100

    adding project on remote rep

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+//f782fa42
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=f782fa42";
+
+function App() {
+  const [movies, setMovies] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const handleClick = () => {
+    if (searchTerm !== "") {
+      searchMovies(searchTerm);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Filmofilija</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={handleClick}
+          aria-disabled
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
